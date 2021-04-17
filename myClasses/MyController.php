8ed5a45ed8c6a83b1d87292cd8f2a90331e7772a
maki_mono <mapooff@gmail.com>
2021-04-17 23:45:02 +0300

    Упростил контроллер

--- myClasses/MyController.php
+++ myClasses/MyController.php
@@ -13,27 +13,24 @@ class MyController
         if (isset($_REQUEST["r"])) {
             $view = $_REQUEST["r"];
 
-            if (in_array($view, ["ladder", "arrays", "phone-disaster"])) {
-                self::runMyView($view);
-            } else {
-                self::runError();
-            }
-            exit;
+            self::runMyView($view);
         }
-
     }
 
-    public static function runMyView($view_name)
+    private static function runMyView($view_name)
     {
         $path = self::VIEW_PATH . "/{$view_name}.php";
         $path = str_replace("/", DIRECTORY_SEPARATOR, $path);
         if (file_exists($path)) {
             echo "<h3><a href='/'><<< НАЗАД</a></h3>";
             include_once($path);
+        } else {
+            self::runError();
         }
+        exit;
     }
 
-    public static function runError()
+    private static function runError()
     {
         echo "<h3><a href='/'><<< НАЗАД</a></h3>";
         echo "<h2>Такой страницы нет!</h2>";
