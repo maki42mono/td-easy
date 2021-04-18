0b13e4496933baa7d5af53b114104360af91011c
maki_mono <mapooff@gmail.com>
2021-04-18 13:45:41 +0300

    Убрал дублирующийся блок НАЗАД

--- myClasses/MyController.php
+++ myClasses/MyController.php
@@ -19,10 +19,11 @@ class MyController
 
     private static function runMyView($view_name)
     {
+        echo "<h3><a href='/'><<< НАЗАД</a></h3>";
+
         $path = self::VIEW_PATH . "/{$view_name}.php";
         $path = str_replace("/", DIRECTORY_SEPARATOR, $path);
         if (file_exists($path)) {
-            echo "<h3><a href='/'><<< НАЗАД</a></h3>";
             include_once($path);
         } else {
             self::runError();
@@ -32,7 +33,6 @@ class MyController
 
     private static function runError()
     {
-        echo "<h3><a href='/'><<< НАЗАД</a></h3>";
         echo "<h2>Такой страницы нет!</h2>";
     }
 }
